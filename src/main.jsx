36dd32f13bc325b46e0294c5b050ccd4f03ab191
Kartik Bora <work.kartikbora@gmail.com>
2026-07-09 07:31:02 +0530

    Add Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-WBV0T2KDSY");
 
 
 createRoot(document.getElementById("root")).render(
